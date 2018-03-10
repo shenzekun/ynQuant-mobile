68c48bfc06497acd94fe9cf820e84f25e9dc4f91
Wanqj <wqj97@126.com>
2018-03-10 18:53:59 +0800

    添加新branch: navigator
    1. 引入新的包: react-navigation
    2. 创建了pages/layout/Navigator.js --- 用于导航的布局
    
    暂时用于学习和测试

--- App.js
+++ App.js
@@ -4,55 +4,27 @@
  * @flow
  */
 
-import React, { Component } from 'react'
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
+import React from 'react'
+import {StackNavigator} from 'react-navigation'
+import HomeScreen from './pages/layout/Navigator'
+import { YellowBox } from 'react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated'
+])
+
+const RootStack = StackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: HomeScreen
+  }
 })
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    )
+export default class App extends React.Component {
+  render () {
+    return <RootStack />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
