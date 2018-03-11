5a7dc3599893b919ca4cbc4b408813d2f7fc1016
Wanqj <wqj97@126.com>
2018-03-11 20:34:26 +0800

    关闭了所有的 warning 警告

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import {YellowBox} from 'react-native'
 import Index from './pages/Layout/Index'
 
-console.dir(YellowBox)
-
-YellowBox.ignoreWarnings([
-  'Warning: componentWillMount is deprecated',
-  'Warning: componentWillReceiveProps is deprecated',
-  'Warning: encourage 1 time'
-])
+console.disableYellowBox = true
+/**
+ * TODO: 配置具体的关闭提醒
+ */
 
 export default class App<Props> extends React.Component {
   render () {
