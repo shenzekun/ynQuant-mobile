d6e14f8743a55513ce5b8fb4f4e9b7bccceb4ca7
Wanqj <wqj97@126.com>
2018-03-11 17:57:07 +0800

    更新了目录结构 添加了setting页面, 添加了react-native-icon库

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import {YellowBox} from 'react-native'
 import Index from './pages/Layout/Index'
 
+console.dir(YellowBox)
+
 YellowBox.ignoreWarnings([
   'Warning: componentWillMount is deprecated',
-  'Warning: componentWillReceiveProps is deprecated'
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: encourage 1 time'
 ])
 
 export default class App<Props> extends React.Component {
--- pages/Layout/Screens.js
+++ pages/Layout/Screens.js
@@ -4,14 +4,34 @@
  */
 
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons' // 图标
 
-import HomeScreen from '../Home/home'
+import HomeScreen from '../Home/home' // 首页
+import Setting from '../Setting/Setting' // 设置
 
 export default {
   Home: {
     screen: HomeScreen,
     navigationOptions: {
       title: '首页',
+      activeTintColor: '#e91e63',
+      labelStyle: {
+        fontSize: 12
+      },
+      showIcon: true,
+      style: {
+        backgroundColor: 'blue'
+      }
+    }
+  },
+  Setting: {
+    screen: Setting,
+    navigationOptions: {
+      tabBarIcon: ({focused, tintColor}) => {
+        console.log(tintColor)
+        return <Ionicons name={'ios-information-circle'} size={25} color={tintColor} />
+      },
+      title: '设置',
       activeTintColor: '#e91e63',
       labelStyle: {
         fontSize: 12
