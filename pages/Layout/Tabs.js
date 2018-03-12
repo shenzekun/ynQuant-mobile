7b9438658d5530e4f78bb2bc4cfc88b196d2c3de
shenzekun <726664809@qq.com>
2018-03-12 08:46:13 +0800

    新增Navigator

--- pages/Layout/Tabs.js
+++ pages/Layout/Tabs.js
@@ -1,18 +1,19 @@
-import {TabNavigator} from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 import React from 'react'
 import Screen from './Screens'
 
-const TabBar = TabNavigator(Screen,
-  {
-    // 定义全局 Tabbar 配置, 配置文档: https://reactnavigation.org/docs/tab-navigator.html
-    initialRouteName: 'Home' // 首页名字
-  }
-)
+const TabBar = TabNavigator(Screen, {
+  // 定义全局 Tabbar 配置, 配置文档: https://reactnavigation.org/docs/tab-navigator.html
+  initialRouteName: 'Home' // 首页名字
+})
 
+const Navigator = StackNavigator({
+  TabBar: {
+    screen: TabBar
+  }
+})
 export default class Tabs extends React.Component {
   render () {
-    return (
-      <TabBar />
-    )
+    return <Navigator />
   }
 }
