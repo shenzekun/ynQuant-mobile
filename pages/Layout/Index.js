c6ae2593eee40aba6419e2845c53b8aac6b48c08
shenzekun <726664809@qq.com>
2018-03-12 08:51:40 +0800

    测试react-native-scrollable-tab-view

--- pages/Layout/Index.js
+++ pages/Layout/Index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Tabs from './Tabs'
 
-export default class App<Props> extends React.Component {
+export default class App extends React.Component {
   render () {
     return (
       <Tabs />
