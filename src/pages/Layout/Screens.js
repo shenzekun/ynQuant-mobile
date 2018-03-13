9b511ec80aea9916b98e54f8753116f66d8e0918
shenzekun <726664809@qq.com>
2018-03-13 20:26:14 +0800

    完成底部 tab

--- src/pages/Layout/Screens.js
+++ src/pages/Layout/Screens.js
@@ -5,41 +5,74 @@
 
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons' // 图标
-
+import { Image } from 'react-native'
 import HomeScreen from '../Home/home' // 首页
-import Setting from '../Setting/setting' // 设置
+import InformationScreen from '../Information/information' // 时讯
+import knowledgeScreen from '../Knowedge/knowledge' // 知识
+import SimulateScreen from '../Simulate/simulate' // 模拟
+import AboutScreen from '../About/about' // 我的
 
 export default {
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      title: '首页',
-      activeTintColor: '#e91e63',
-      labelStyle: {
-        fontSize: 12
-      },
-      showIcon: true,
-      style: {
-        backgroundColor: 'blue'
-      }
-    }
-  },
-  Setting: {
-    screen: Setting,
+  knowledgeScreen: {
+    screen: knowledgeScreen,
     navigationOptions: {
       tabBarIcon: ({ focused, tintColor }) => {
-        console.log(tintColor)
-        return <Ionicons name={'ios-information-circle'} size={25} color={tintColor} />
+        return (
+          <Image
+            source={
+              focused
+                ? require('../../images/knowledge-active.png')
+                : require('../../images/knowledge.png')
+            }
+          />
+        )
       },
-      title: '设置',
-      activeTintColor: '#e91e63',
-      labelStyle: {
-        fontSize: 12
+      title: '知识'
+    }
+  },
+  InformationScreen: {
+    screen: InformationScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused, tintColor }) => {
+        return (
+          <Image
+            source={
+              focused ? require('../../images/shape-active.png') : require('../../images/shape.png')
+            }
+          />
+        )
       },
-      showIcon: true,
-      style: {
-        backgroundColor: 'blue'
-      }
+      title: '时讯'
+    }
+  },
+  SimulateScreen: {
+    screen: SimulateScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused, tintColor }) => {
+        return (
+          <Image
+            source={
+              focused ? require('../../images/simulate-active.png') : require('../../images/simulate.png')
+            }
+          />
+        )
+      },
+      title: '模拟'
+    }
+  },
+  AboutScreen: {
+    screen: AboutScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused, tintColor }) => {
+        return (
+          <Image
+            source={
+              focused ? require('../../images/about-active.png') : require('../../images/about.png')
+            }
+          />
+        )
+      },
+      title: '我的'
     }
   }
 }
--- src/pages/Layout/Tabs.js
+++ src/pages/Layout/Tabs.js
@@ -1,10 +1,24 @@
+/**
+  navigation 详细说明
+  http://www.guiyongdong.com/2017/05/20/ReactNative%E5%AF%BC%E8%88%AA%E6%96%B0%E5%AE%A0%E5%84%BFreact-navigation/
+*/
 import { TabNavigator, StackNavigator } from 'react-navigation'
 import React from 'react'
 import Screen from './Screens'
 
 const TabBar = TabNavigator(Screen, {
   // 定义全局 Tabbar 配置, 配置文档: https://reactnavigation.org/docs/tab-navigator.html
-  initialRouteName: 'Home' // 首页名字
+  initialRouteName: 'InformationScreen', // 首页名字
+  tabBarOptions: {
+    style: {
+      backgroundColor: '#000' // 底部 tab 颜色
+    },
+    activeTintColor: '#fff', // 选中时文字颜色
+    inactiveTintColor: '#9a9a9a', // 未选中时文字颜色
+    showIcon: true,
+    lazy: true // 是否懒加载界面，默认一次加载所有的界面,true为懒加载
+  },
+  animationEnabled: false // 开启动画
 })
 
 const Navigator = StackNavigator({
