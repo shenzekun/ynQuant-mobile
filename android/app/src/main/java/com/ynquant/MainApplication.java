2112f4d70da7e2e1bff0781767acd719d398ec36
shenzekun <726664809@qq.com>
2018-03-15 15:22:41 +0800

    修复重大 bug

--- android/app/src/main/java/com/ynquant/MainApplication.java
+++ android/app/src/main/java/com/ynquant/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.wix.interactable.Interactable;
-import com.wix.interactable.Interactable;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new Interactable(),
             new Interactable(),
             new VectorIconsPackage()
       );
