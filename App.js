d7c77e51d7a3a68b32c4b336a5d93df5c30e7db4
yxl <yangxulei84@qq.com>
2018-03-16 13:12:04 +0800

    修改入口视图

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default class App extends React.Component {
   render () {
     return (
       <Provider store={configureStore}>
-        {/* <Navigator /> */}
-        <RegisterScreen />
+        <Navigator />
+        {/* <RegisterScreen /> */}
       </Provider>
     )
   }
